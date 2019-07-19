2fba2a067780b34647d09fd32c6e6ca9166d1af0
Horace Velmont <dragond7@naver.com>
2019-07-19 23:54:45 +0900

    L2 (failed)

---
+++ src/main/scala/leetcode/L2.scala
@@ -0,0 +1,12 @@
+package leetcode
+
+class ListNode(var _x: Int = 0) {
+  var next: ListNode = null
+  var x: Int = _x
+}
+
+object Solution {
+  def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
+    nulls
+  }
+}
